f23bdfa835f3efb0fbfb4be8ea6593c81f4ecc4c
playwhyyza <poton_jom@outlook.com>
2025-06-09 23:50:53 +0700

    feat: add loop, continue, break, match, and handling invalid input

---
+++ 02-generating-a-secret-number/generate-secret-number/src/main.rs
@@ -0,0 +1,38 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, // value will match the first arm's pattern
+            Err(_) => continue, // which tells the programe to go the next iteration of the loop
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
+}
