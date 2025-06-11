65bf34c48754e78517d19e1ff5d9f90f0b5e69eb
playwhyyza <poton_jom@outlook.com>
2025-06-11 16:57:38 +0700

    feat: add tuple and array

--- 03-common-programming-concepts/data-types/src/main.rs
+++ 03-common-programming-concepts/data-types/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     /*
         Scalar
@@ -103,9 +105,89 @@ fn main() {
             - arrays
     */
 
-    // The Tuple Type
+    /* 
+        The Tuple Type 
+        
+        a tuple is a general way of grouping together a number of values with a variety of types into one compound type
+        tuples have a fixed length: once declared, they cannot grow or shrink in size
+
+        tuple element directly by suing a period (.) followed by the index of the value
+
+        the first in a tuple is 0
+    */
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let _tup_2 = (500, 6.4, 1, 'c', "Thai");
+    let (_x, y, _z, c, s) = _tup_2;
+
+    println!("The value of y is: {y}");
+    println!("The value of c is: {c}");
+    println!("The value of s is: {s}");
     
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
 
-    // The Array Type
+    println!("The value of five_hundred is: {five_hundred}");
+    println!("The value of six_point_four is: {six_point_four}");
+    println!("The value of one is: {one}");
 
+    /* 
+        Array Type 
+
+        another way to collection of multiple values is with an array.
+        every element of an array must have the same type.
+        array in Rust have a fixed length.
+
+        However, arrays are more useful when you know the number of elements will not need to change.
+
+        sample:
+            let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+            i32 is the type of each element. after the semicolon
+            the 5 indicates the array contains five elements
+
+
+            let a = [3; 5] 
+
+            the array named a will contain 5 elements that will all be set to the value 3 initially.
+            this is the same as writing let a = [3, 3, 3, 3, 3]
+    */
+
+    let a = [1, 2, 3, 4, 5];
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+    let c = [3; 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+                    "August", "September", "October", "November", "December"];
+
+    let first = a[0];
+    let second = a[1];
+
+    println!("The value of first is: {first}");
+    println!("The value of second is: {second}");
+
+    println!("The value of a is: {}", a[3]);
+    println!("The value of b is: {}", b[4]);
+    println!("The value of c is: {}", c[3]);
+    println!("The value of months is: {}", months[8]);
+
+    let length_of_a: usize = a.len();
+    println!("The value of length a is: {length_of_a}");
+
+    println!("Please enter an array index. between 0 to 4 ?");
+    
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+    println!("The value of the element at index {index} is: {element}");
+    
 }
