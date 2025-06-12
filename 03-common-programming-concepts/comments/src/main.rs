1cd3b938ec23aee27db7988dc2bfbc329ce28d8e
playwhyyza <poton_jom@outlook.com>
2025-06-12 11:27:19 +0700

    feat: comments

---
+++ 03-common-programming-concepts/comments/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    // So we're doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what's going on.
+
+    let lucky_number = 7; // I'm feeling lucky today
+    println!("the value of lucky_number is: {lucky_number}");
+}
