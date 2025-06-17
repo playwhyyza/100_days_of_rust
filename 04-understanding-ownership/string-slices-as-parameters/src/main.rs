cacf3f6ba7f28eef755b12ab797ecda516e980dd
playwhyyza <poton_jom@outlook.com>
2025-06-17 22:31:12 +0700

    learn: string slices as parameters

---
+++ 04-understanding-ownership/string-slices-as-parameters/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole.
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+
+    // `first_word` also works on reference to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole.
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this work too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    /*
+        Other slices
+
+        This slice has the type `&[i32]`. It workks the same way as string slices, by storing a reference to
+        the first element and a length. You'll use this kind of slice for all sorts of other collections.
+     */
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
